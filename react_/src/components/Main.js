0201bc716953811bfdbabaa5a747cc1414bdf580
AleksandrMyronow <a.myronow@gmail.com>
2018-02-17 14:40:42 +0200

    solving with router

--- react_/src/components/Main.js
+++ react_/src/components/Main.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import App from './App'
+// import App from './App'
 import Form from '../containers/Form'
 import Articles from '../containers/Articles'
 import Registration from '../containers/Login/Registration'
+import Home from '../components/Home'
 
 const Main = () => (
     <main>
         <Switch>
-            <Route exact path='/' component={App}/>
+            <Route exact path='/' component={Home}/>
             <Route path='/form' component={Form}/>
             <Route path='/registration' component={Registration}/>
             <Route path='/articles' component={Articles}/>
