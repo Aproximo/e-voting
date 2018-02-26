ef7bbf2489431a7d0da15695dc93d6243e2f1c4f
AleksandrMyronow <a.myronow@gmail.com>
2018-02-26 17:25:58 +0200

    do not work candidates API

--- react_/src/components/Main.js
+++ react_/src/components/Main.js
@@ -8,11 +8,11 @@ import Voting from "../containers/Voting/Voting";
 import Registration, {PrivateRoute} from "../containers/Login/Registration";
 import Home from '../components/Home';
 import ArticleRoutes from "../containers/Articles/Routes";
-import Routes from "../containers/Candidates/Routes";
+import CandidatRoutes from "../containers/Candidates/Routes";
 import Personal from '../containers/Personal/Personal';
 import Articles from '../containers/Articles/Articles';
 import Articlе from '../containers/Articles/Article';
-
+import Candidat from "../containers/Candidates/Candidat";
 import Form from '../containers/Form/Form';
 import Admin from '../components/Admin';
 import '../style/components/Main.css';
@@ -30,9 +30,10 @@ class Main extends React.Component {
                     <Route path='/articles' component={ArticleRoutes}/>
                     <Route path='/article/:id' component={Articlе}/>
                     <Route path='/voting' component={Voting}/>
-                    <Route path='/presidential-elections' component={Routes}/>
                     <Route path='/admin' component={Admin}/>
                     <Route path='/personal' component={Personal}/>
+                    <Route path='/candidates' component={CandidatRoutes}/>
+                    <Route path='/candidat/:id' component={Candidat}/>
                 </Switch>
             </main>
         )
