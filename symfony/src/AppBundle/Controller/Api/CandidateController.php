9622558faf192b038cd1463c252a499be092d4a2
Aproximo <Romanyk94@gmail.com>
2018-02-26 15:36:43 +0200

    add Candidate rout

---
+++ symfony/src/AppBundle/Controller/Api/CandidateController.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: or_os
+ * Date: 20.11.2017
+ * Time: 14:56
+ */
+
+namespace AppBundle\Controller\Api;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Accounts;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController extends Controller
+{
+
+    /**
+     * article list page
+     *
+     * @Route("/api/candidate", name="api_article")
+     *
+     */
+    public function indexAction(){
+        header("Access-Control-Allow-Origin: *");
+
+        $repo = $this->get('doctrine')->getRepository('AppBundle:Candidate');
+        $articles = $repo->findAll();
+
+        $array = $this->get('jms_serializer')->serialize($articles, 'json');
+
+
+
+        return new JsonResponse($array);
+    }
+
+    /**
+     *
+     * article by id
+     * sl - for tralling slash if its needed
+     *
+     * @Route("/api/candidate/{id}{sl}", name="api_article_page", defaults={"sl" : ""}, requirements={"id" : "[1-9][0-9]*","sl":"/?"})
+     *
+     */
+    public function showAction($id){
+        header("Access-Control-Allow-Origin: *");
+
+        $repo = $this->get('doctrine')->getRepository('AppBundle:Candidate');
+        $article = $repo->find($id);
+
+        if(!$article){
+            return new JsonResponse("NotFound",404);
+        }
+
+        $array = $this->get('jms_serializer')->serialize($article, 'json');
+
+
+
+        return new JsonResponse($array);
+    }
+
+
+
+}
